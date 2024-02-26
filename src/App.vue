c17bb9009e74005ea401d4afcb8f28d8c6f45d99
timchao <ptp_timchao@photons.com.tw>
2024-02-26 16:24:49 +0800

    update text 444

--- src/App.vue
+++ src/App.vue
@@ -8,7 +8,7 @@ import HelloWorld from './components/HelloWorld.vue'
     <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
 
     <div class="wrapper">
-      <HelloWorld msg="ci cd test 333" />
+      <HelloWorld msg="ci cd test 444" />
 
       <nav>
         <RouterLink to="/">Home</RouterLink>
